dea3370fe98cd9c8a79344f580f463fb114c5109
Andrew Merz <andrewmerz@Andrews-MacBook-Pro.local>
2017-07-14 16:18:10 -0700

    img added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
+var game = new Phaser.Game(900, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
 
 
 function preload(){
+  var img1 = game.load.image('phaser', 'assets/backgrounds/phaser_img.png');
 
 };
 function create(){
-  game.stage.backgroundColor = "#4BBBA";
+  //game.stage.backgroundColor = "#FFFFF";
+  game.add.sprite(0,0,'phaser');
+
 };
 function update(){
 
